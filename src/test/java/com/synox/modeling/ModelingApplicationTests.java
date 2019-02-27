f6c62de9632d01c7e1cf895b7a1c6308f56d4936
Herbert <sousacruz@hotmail.com>
2019-02-26 23:56:33 -0300

    Project created

---
+++ src/test/java/com/synox/modeling/ModelingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.synox.modeling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
