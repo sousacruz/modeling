1726b652d0a0826dc2afaef96f54ab39237bb672
Herbert <sousacruz@hotmail.com>
2019-03-01 07:59:11 -0300

    Customizing Category validation error messages

--- src/main/java/com/synox/modeling/resources/ResourceExceptionHandler.java
+++ src/main/java/com/synox/modeling/resources/ResourceExceptionHandler.java
@@ -4,12 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.synox.modeling.services.exception.DataIntegrityException;
 import com.synox.modeling.services.exception.ObjectNotFoundException;
 import com.synox.modeling.services.exception.StandardError;
+import com.synox.modeling.services.exception.ValidationError;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -21,8 +24,19 @@ public class ResourceExceptionHandler {
 	}
 
 	@ExceptionHandler(DataIntegrityException.class)
-	public ResponseEntity<StandardError> daataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Validation error!", System.currentTimeMillis());
+		for (FieldError fe : e.getBindingResult().getFieldErrors()) {
+			err.addError(fe.getField(), fe.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
+
+
